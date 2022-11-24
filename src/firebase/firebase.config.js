4a184f5e43a2ef12873a1c2d236d68abee02985e
Rifat <mdrifatahmed787@gmail.com>
2022-11-25 00:11:39 +0600

    firebase secured

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBbg_mSctqDm6kKdxHjWCQU7xsXHAnaDoM",
-  authDomain: "resale-products-fa629.firebaseapp.com",
-  projectId: "resale-products-fa629",
-  storageBucket: "resale-products-fa629.appspot.com",
-  messagingSenderId: "251557206920",
-  appId: "1:251557206920:web:c9ef5e1005c2f93d20b83d",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
