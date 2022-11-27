8249ab153c7ad93596c9abfe95c2cd3dda598bf1
Rifat <mdrifatahmed787@gmail.com>
2022-11-28 05:08:35 +0600

    modal btn designed

--- src/layout/DashboardLayout.js
+++ src/layout/DashboardLayout.js
@@ -22,7 +22,7 @@ const DashboardLayout = () => {
         </div>
         <div className="drawer-side">
           <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-80 bg-base-100 rounded-lg lg:bg-none  text-base-content mt-11">
+          <ul className="menu p-4 w-80  rounded-lg lg:bg-none  text-base-content mt-11">
             <li className="mb-3">
               <Link to="/dashboard">My Orders</Link>
             </li>
--- src/pages/Dashboard/MyOrders/MyOrdersModal.js
+++ src/pages/Dashboard/MyOrders/MyOrdersModal.js
@@ -11,19 +11,19 @@ const MyOrdersModal = ({
       <input type="checkbox" id="orderdelete-modal" className="modal-toggle" />
       <div className="modal">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">{title}</h3>
-          <div className="modal-action">
+          <h3 className="font-bold text-lg text-center">{title}</h3>
+          <div className="modal-action flex justify-center">
             <label
               onClick={() => handleDeleteOrder(modalData)}
               htmlFor="orderdelete-modal"
-              className="btn"
+              className="btn btn-sm btn-error"
             >
               Delete
             </label>
             <label
               onClick={handleCancel}
               htmlFor="orderdelete-modal"
-              className="btn"
+              className="btn btn-sm btn-primary"
             >
               Cancel
             </label>
